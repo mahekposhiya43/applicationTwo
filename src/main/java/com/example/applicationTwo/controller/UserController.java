865cf36333b6c67782d11ca36ec34fb4ad89145e
mahek <mahekposhiya@gmail.com>
2025-03-17 06:57:21 +0530

    application 2 created

--- src/main/java/com/example/applicationTwo/controller/UserController.java
+++ src/main/java/com/example/applicationTwo/controller/UserController.java
@@ -1,9 +1,24 @@
 package com.example.applicationTwo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
+    @GetMapping("/user")
+    public ResponseEntity<String> getUserData(@AuthenticationPrincipal Jwt jwt) {
+        return ResponseEntity.ok("Hello, " + jwt.getClaim("preferred_username"));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public ResponseEntity<String> getAdminData() {
+        return ResponseEntity.ok("Admin access granted!");
+    }
 }
